319d69adb0595eb613715ee8d1e0e3f9374c2735
baeminsoo <baeminsoo1@gmail.com>
2026-07-18 00:41:13 +0900

    feat: DataTypes알아보기

---
+++ src/_02_DataTypes.java
@@ -0,0 +1,14 @@
+import java.nio.channels.AsynchronousByteChannel;
+
+public class _02_DataTypes {
+    public static void main(String[] args) {
+        System.out.println("안녕하세요");
+        System.out.println("Hello World!!");
+        System.out.println(11);
+        System.out.println(11.523);
+        System.out.println(10 + 11);
+        System.out.println(true);
+        System.out.println("true");
+        System.out.println(false);
+    }
+}
