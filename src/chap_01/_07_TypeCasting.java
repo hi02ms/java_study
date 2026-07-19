6105922c44c1cb17f3ed7bac279d581458c97ebf
baeminsoo <baeminsoo1@gmail.com>
2026-07-20 01:57:48 +0900

    feat: 형 변환 예제 작성

--- src/chap_01/_07_TypeCasting.java
+++ src/chap_01/_07_TypeCasting.java
@@ -1,4 +1,16 @@
 package chap_01;
 
 public class _07_TypeCasting {
+    public static void main(String[] args) {
+
+        // int to float, double
+        int score = 90;
+        System.out.println(score);
+        System.out.println((float) score);
+        System.out.println((double) score);
+
+        // float, double to int
+        float score_f = 93.3F;
+        double score_d = 98.8;
+    }
 }
