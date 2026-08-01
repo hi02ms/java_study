b537c152377f63b6bc5baae845b072e903b5cad8
baeminsoo <baeminsoo1@gmail.com>
2026-08-02 08:39:38 +0900

    feat: 쿠폰 입력 및 할인 기능 구현

--- src/chap01/OrderCalculator.java
+++ src/chap01/OrderCalculator.java
@@ -81,6 +81,26 @@ public class OrderCalculator {
         }
     }
 
+    // 쿠폰 정책
+    public static String readCouponCode(Scanner scanner) {
+
+        System.out.print("쿠폰 코드를 입력하세요 (없으면 Enter): ");
+        String couponCode = scanner.nextLine().strip();
+
+        // 빈입력 : 쿠폰 사용 안함
+        if (couponCode.isBlank()){
+            return "";
+        }
+        // 유효한 쿠폰
+        if (couponCode.equalsIgnoreCase("SAVE3000")) {
+            return "SAVE3000";
+        } else {
+            // 잘못된 쿠폰
+            System.out.println("유효하지 않은 쿠폰입니다. 쿠폰 없이 진행합니다.");
+            return "";
+        }
+    }
+
     // main
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -89,11 +109,18 @@ public class OrderCalculator {
         long unitPrice = readUnitPrice(scanner);    // 상품 단가 확인
         int quantity = readQuantity(scanner);       // 주문 수량 확인
         boolean isMember = readMembership(scanner); // 회원 여부 확인
+        String couponCode = readCouponCode(scanner); // 쿠폰 여부 확인
+        boolean hasCoupon = couponCode.equalsIgnoreCase("SAVE3000"); // 이부분 true 인지 확인..
+
 
         // 계산 단계
         long totalPrice = unitPrice * quantity; // Java의 산술 연산에서는 두 피연산자의 자료형이 다르면 더 큰 자료형에 맞춰서 계산. long과 int를 곱하면 int가 long으로 자동 승격되어 결과도 long이 된다.
         long memberDiscount = isMember ? totalPrice / 10 : 0; // 회원할인 = 상품금액 / 10
-        long discountedPrice = totalPrice - memberDiscount;
+        long priceAfterMemberDiscount = totalPrice - memberDiscount;
+        boolean isCouponApplicable = hasCoupon && priceAfterMemberDiscount >= 30_000L;
+        long couponDiscount = isCouponApplicable ? 3_000L : 0L;
+        long priceAfterDiscounts = priceAfterMemberDiscount - couponDiscount;
+
 
         // 출력 단계
         System.out.println();
@@ -108,8 +135,20 @@ public class OrderCalculator {
             System.out.printf("회원 여부: 비회원%n");
             System.out.printf("회원 할인: %d원%n", memberDiscount);
         }
-        System.out.printf("할인 후 금액: %,d원%n", discountedPrice);
+        System.out.printf("회원 할인 후 금액: %,d원%n", priceAfterMemberDiscount);
+        if (isCouponApplicable) {
+            System.out.println("쿠폰: " + couponCode);
+            System.out.printf("쿠폰 할인: -%,d원%n", couponDiscount);
+        } else if (hasCoupon) {
+            System.out.println("쿠폰: " + couponCode + " 적용 조건 미충족");
+            System.out.println("쿠폰은 회원 할인 후 금액이 30,000원 이상일 때 사용할 수 있습니다.");
+            System.out.printf("쿠폰 할인: %,d원%n", couponDiscount);
+        } else {
+            System.out.println("쿠폰: 사용 안함");
+            System.out.printf("쿠폰 할인: %,d원%n", couponDiscount);
+        }
 
+        System.out.printf("모든 할인 후 금액: %,d원%n", priceAfterDiscounts);
         scanner.close();
     }
 }
